d327e7200f0f1f5975150660bd34a1e6e2246b62
lilitantonyan <lilit@helix.am>
2025-01-15 21:36:23 +0400

    add home page

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(function (){
+    $('.has-sub').on('click',function (e){
+        e.preventDefault();
+        $('.sub-menu-content').css({ "max-height": "0px" });
+        $('.sub-menu-content').addClass('collapsed');
+        let w = $(this).siblings('.sub-menu-content');
+        let inner = $(this).siblings('.sub-menu-content').find('.sub-menu')
+        if (w.hasClass('collapsed')) {
+            console.log('aaaa')
+            w.css({ "max-height": inner.outerHeight() + 'px' });
+
+            w.removeClass('collapsed');
+        } else {
+            w.css({ "max-height": "0px" });
+
+        }
+    })
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 120) {
+            $('nav').addClass('sticky');
+        }
+        if ($(window).scrollTop() < 120) {
+            $('nav').removeClass('sticky');
+        }
+    });
+})
