28a8121a9f746fd8239d79ddfbf41229d01ef610
lilitantonyan <lilit@helix.am>
2025-01-17 23:30:17 +0400

    add home page

--- js/main.js
+++ js/main.js
@@ -2,17 +2,20 @@ $(function (){
     $('.has-sub').on('click',function (e){
         e.preventDefault();
         $('.sub-menu-content').css({ "max-height": "0px" });
-        $('.sub-menu-content').addClass('collapsed');
         let w = $(this).siblings('.sub-menu-content');
+        w.addClass('clicked')
+        $('.sub-menu-content:not(.clicked)').addClass('collapsed');
         let inner = $(this).siblings('.sub-menu-content').find('.sub-menu')
         if (w.hasClass('collapsed')) {
             console.log('aaaa')
             w.css({ "max-height": inner.outerHeight() + 'px' });
 
             w.removeClass('collapsed');
+            w.removeClass('clicked')
         } else {
             w.css({ "max-height": "0px" });
-
+            w.addClass('collapsed');
+            w.removeClass('clicked')
         }
     })
     $(window).scroll(function () {
@@ -24,9 +27,17 @@ $(function (){
         }
     });
     $('.btn-menu').on('click',function (){
-        $('.nav-content').toggle();
-        $(this).toggleClass('open');
+       if(!$('.nav-content').hasClass('open')){
+           console.log('asdasda')
+           $('.nav-content').addClass('open');
+           $('.nav-content').css({ "max-height": $('.nav-content').find('.menu').outerHeight() + 'px' });
+       } else {
+           $('.nav-content').removeClass('open');
+           $('.nav-content').css({ "max-height": "0px" });
+       }
+
         $('.header-lang').toggleClass('open');
         $('.header-top-left').toggleClass('open');
     })
+
 })
